efb47cc25c183dd6b785bce5ec2219b5626cff5c
unknown <mateus@mateuskoehler.com>
2019-11-22 14:41:37 -0500

    basic visuals

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import { Typography, AppBar, Toolbar } from "@material-ui/core";
+import Switch from "./components/Switch";
+import Card from "./components/Card";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    minHeight: "100vh"
+  },
+  title: {
+    marginRight: "1em"
+  },
+  content: {
+    marginTop: "2em",
+    flexGrow: 1
+  }
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      {/* Appbar */}
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Appbar
+          </Typography>
+          <Switch />
+        </Toolbar>
+      </AppBar>
+      {/* Main content */}
+      <Grid
+        container
+        spacing={2}
+        direction="row"
+        justify="center"
+        alignItems="center"
+        className={classes.content}
+      >
+        {[0, 1, 2, 3].map(value => (
+          <Grid key={value} item>
+            <Card value={value} />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 250
+  },
+  media: {
+    height: 140
+  }
+});
+
+export default function MediaCard(props) {
+  const classes = useStyles();
+  const { value } = props;
+  return (
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="h2">
+          Crypto
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {value}
+        </Typography>
+      </CardContent>
+      <CardMedia
+        className={classes.media}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="Contemplative Reptile"
+      />
+    </Card>
+  );
+}
